2d630c70c3984627196990875423c3bf695f5878
Jason Coleman <jason@strangerstudios.com>
2021-03-13 09:03:13 +1100

    NOTE: This version requires PMPro version 2.5.8 or higher. * BUG FIX/ENHANCEMENT: Uses the new pmpro_change_level filter to keep the user's level from changing ever. Their order and subsciptions are cancelled and their expiration date is set up, but the pmpro_before_change_membership_level and pmpro_after_change_membership_level will not fire. This prevents issues with other code hooked into membership level changes.

--- pmpro-cancel-on-next-payment-date.php
+++ pmpro-cancel-on-next-payment-date.php
@@ -3,7 +3,7 @@
  Plugin Name: Paid Memberships Pro - Cancel on Next Payment Date
  Plugin URI: https://www.paidmembershipspro.com/add-ons/cancel-on-next-payment-date
  Description: Change membership cancellation to set expiration date for next payment instead of cancelling immediately.
- Version: 0.3
+ Version: 0.4
  Author: Paid Memberships Pro
  Author URI: https://www.paidmembershipspro.com
  Text Domain: pmpro-cancel-on-next-payment-date
@@ -19,14 +19,14 @@ function pmproconpd_load_text_domain() {
 add_action( 'plugins_loaded', 'pmproconpd_load_text_domain' );
 
 /**
- * Before cancelling, save the next_payment_timestamp to a global for later use.
- *
+ * If the user has a payment coming up, don't cancel.
+ * Instead update their expiration date and keep their level.
  */
-function pmproconpd_pmpro_before_change_membership_level( $level_id, $user_id, $old_levels, $cancel_level ) {
-	global $pmpro_pages, $wpdb, $pmpro_next_payment_timestamp;
-
-	// Are we on the cancel page and cancelling a level?
-	if ( $level_id == 0 && ( is_page( $pmpro_pages['cancel'] ) || ( is_admin() && ( empty($_REQUEST['from'] ) || $_REQUEST['from'] != 'profile' ) ) ) ) {
+function pmproconpd_pmpro_change_level( $level, $user_id, $old_level_status, $cancel_level ) {
+    global $pmpro_pages, $wpdb, $pmpro_next_payment_timestamp;
+    
+    // Are we on the cancel page and cancelling a level?
+	if ( $level == 0 && ( is_page( $pmpro_pages['cancel'] ) || ( is_admin() && ( empty($_REQUEST['from'] ) || $_REQUEST['from'] != 'profile' ) ) ) ) {
 		// Default to false. In case we're changing membership levels multiple times during this page load.
 		$pmpro_next_payment_timestamp = false;
 
@@ -34,13 +34,13 @@ function pmproconpd_pmpro_before_change_membership_level( $level_id, $user_id, $
 		$order = new MemberOrder();
 		$order->getLastMemberOrder( $user_id, 'success', $cancel_level );
 
-		// Get level to check, if it already has an end date.
+		// Get level to check if it already has an end date.
 		if ( ! empty( $order ) && ! empty ( $order->membership_id ) ) {
-			$level = $wpdb->get_row("SELECT * FROM $wpdb->pmpro_memberships_users WHERE membership_id = '" . $order->membership_id . "' AND user_id = '" . $user_id . "' ORDER BY id DESC LIMIT 1");
+			$check_level = $wpdb->get_row("SELECT * FROM $wpdb->pmpro_memberships_users WHERE membership_id = '" . $order->membership_id . "' AND user_id = '" . $user_id . "' ORDER BY id DESC LIMIT 1");
 		}
 
 		// Figure out the next payment timestamp.
-		if ( empty( $level ) || ( ! empty( $level->enddate ) && $level->enddate != '0000-00-00 00:00:00' ) ) {
+		if ( empty( $check_level ) || ( ! empty( $check_level->enddate ) && $check_level->enddate != '0000-00-00 00:00:00' ) ) {
 			// Level already has an end date. Set to false so we really cancel.
 			$pmpro_next_payment_timestamp = false;
 		} elseif ( ! empty( $order ) && $order->gateway == 'stripe' ) {
@@ -68,87 +68,29 @@ function pmproconpd_pmpro_before_change_membership_level( $level_id, $user_id, $
 			$pmpro_next_payment_timestamp = pmpro_next_payment( $user_id );
 		}
 	}
+
+    // Are we extending?
+    if ( ! empty( $pmpro_next_payment_timestamp ) ) {        
+        // Make sure they keep their level.
+        $level = $cancel_level;
+        
+        // Cancel their last order.
+        if ( ! empty( $order ) ) {
+            $order->cancel();
+        }
+        
+        // Update the expiration date.
+        $expiration_date = date('Y-m-d H:i:s', $pmpro_next_payment_timestamp );
+        $sqlQuery = "UPDATE $wpdb->pmpro_memberships_users SET enddate = '" . esc_sql( $expiration_date ) . "' WHERE status = 'active' AND membership_id = '" . esc_sql( $level ) . "' AND user_id = '" . esc_sql( $user_id ) . "' LIMIT 1";
+        $wpdb->query( $sqlQuery );
+        
+        // Change the message shown on Cancel page.
+	    add_filter( 'gettext', 'pmproconpd_gettext_cancel_text', 10, 3 );
+    }
+    
+    return $level;
 }
-add_action( 'pmpro_before_change_membership_level', 'pmproconpd_pmpro_before_change_membership_level', 10, 4 );
-
-/**
- * Give users their level back with an expiration.
- *
- */
-function pmproconpd_pmpro_after_change_membership_level( $level_id, $user_id ) {
-	global $pmpro_pages, $wpdb, $pmpro_next_payment_timestamp;
-
-	if ( $pmpro_next_payment_timestamp != false && 		//this is false if the level already has an enddate
-	   $level_id == 0 && 								//make sure we're cancelling
-	   ( is_page($pmpro_pages['cancel'] ) || (is_admin() && ( empty($_REQUEST['from'] ) || $_REQUEST['from'] != 'profile' ) ) ) ) { //on the cancel page or in admin/adminajax/webhook and not the edit user page
-		/**
-		 * Okay, let's give the user their old level back with an expiration
-		 * based on their subscription date.
-		 *
-		 */
-		// Get the last order.
-		$order = new MemberOrder();
-		$order->getLastMemberOrder( $user_id, 'cancelled' );
-
-		// We can't do this if we can't find the order.
-		if ( empty( $order->id ) ) {
-			return false;
-		}
-
-		// Get the last level the user had.
-		$level = $wpdb->get_row("SELECT * FROM $wpdb->pmpro_memberships_users WHERE membership_id = '" . $order->membership_id . "' AND user_id = '" . $user_id . "' ORDER BY id DESC LIMIT 1");
-
-		// We can't do this if we can't find an old level.
-		if ( empty( $level ) ) {
-			return false;
-		}
-
-		// Get the last payment date.
-		$lastdate = date( 'Y-m-d', $order->timestamp );
-
-		// Get the next payment date.
-		if ( ! empty( $pmpro_next_payment_timestamp ) ) {
-			$nextdate = $pmpro_next_payment_timestamp;
-		} else {
-			// TODO Update to use WP time.
-			$nextdate = $wpdb->get_var("SELECT UNIX_TIMESTAMP('" . $lastdate . "' + INTERVAL " . $level->cycle_number . " " . $level->cycle_period . ")");
-		}
-
-		// Run the process to add level back if the date is in the future.
-		if ( $nextdate - current_time( 'timestamp' ) > 0 ) {
-			// Give them their level back with the expiration date set.
-			$old_level = $wpdb->get_row("SELECT * FROM $wpdb->pmpro_memberships_users WHERE membership_id = '" . $order->membership_id . "' AND user_id = '" . $user_id . "' ORDER BY id DESC LIMIT 1", ARRAY_A);
-			$old_level['enddate'] = date( 'Y-m-d H:i:s', $nextdate );
-
-			// Disable this hook so we don't loop.
-			remove_action( 'pmpro_before_change_membership_level', 'pmproconpd_pmpro_before_change_membership_level', 10, 4 );
-			remove_action( 'pmpro_after_change_membership_level', 'pmproconpd_pmpro_after_change_membership_level', 10, 2 );
-
-			/**
-			 * Disable the action to set the default level on cancels
-			 * to make compatible with this previous gist https://gist.github.com/strangerstudios/5703500
-			 *
-			 */
-			remove_action( 'pmpro_after_change_membership_level', 'pmpro_after_change_membership_level_default_level', 10, 2 );
-
-			// Change the level.
-			pmpro_changeMembershipLevel( $old_level, $user_id );
-
-			// Add the action back just in case.
-			add_action( 'pmpro_before_change_membership_level', 'pmproconpd_pmpro_before_change_membership_level', 10, 4 );
-			add_action( 'pmpro_after_change_membership_level', 'pmproconpd_pmpro_after_change_membership_level', 10, 2 );
-
-			// Add the action back to set the default level on cancels.
-			if ( function_exists( 'pmpro_after_change_membership_level_default_level' ) ) {
-				add_action( 'pmpro_after_change_membership_level', 'pmpro_after_change_membership_level_default_level', 10, 2 );
-			}
-
-			// Change the message shown on Cancel page.
-			add_filter( 'gettext', 'pmproconpd_gettext_cancel_text', 10, 3 );
-		}
-	}
-}
-add_action( 'pmpro_after_change_membership_level', 'pmproconpd_pmpro_after_change_membership_level', 10, 2 );
+add_filter( 'pmpro_change_level', 'pmproconpd_pmpro_change_level', 10, 4 );
 
 /**
  * Replace the cancellation text so people know they'll still have access for a certain amount of time.
@@ -172,7 +114,6 @@ function pmproconpd_gettext_cancel_text( $translated_text, $text, $domain ) {
 
 /**
  * Update the cancellation email text so people know they'll still have access for a certain amount of time.
- *
  */
 function pmproconpd_pmpro_email_body( $body, $email ) {
 	global $pmpro_next_payment_timestamp;
@@ -180,7 +121,6 @@ function pmproconpd_pmpro_email_body( $body, $email ) {
 	/**
 	 * Only filter the 'cancel' template and
 	 * double check that we have reinstated their membership through this Add On.
-	 *
 	 */
 	if ( $email->template == 'cancel' && ! empty( $pmpro_next_payment_timestamp ) ) {
 		global $wpdb;
@@ -200,7 +140,6 @@ add_filter( 'pmpro_email_body', 'pmproconpd_pmpro_email_body', 10, 2 );
 
 /**
  * Function to add links to the plugin row meta.
- *
  */
 function pmproconpd_plugin_row_meta( $links, $file ) {
 	if ( strpos( $file, 'pmpro-cancel-on-next-payment-date.php' ) !== false ) {
