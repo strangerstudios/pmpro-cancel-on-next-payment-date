21f3d12a37504eafd549d038a013c1e4b325a688
Mirco Babini <mirkolofio@gmail.com>
2022-07-13 23:19:35 +0200

    Add support for PayPal Standard (deprecated) and add a missing PPE case to immediately cancel

--- pmpro-cancel-on-next-payment-date.php
+++ pmpro-cancel-on-next-payment-date.php
@@ -83,11 +83,20 @@ function pmproconpd_pmpro_change_level( $level, $user_id, $old_level_status, $ca
 		$pmpro_next_payment_timestamp = false;
 	} elseif ( ! empty( $order ) && 'stripe' === $order->gateway ) {
 		$pmpro_next_payment_timestamp = PMProGateway_stripe::pmpro_next_payment( '', $user_id, 'success' );
+	} elseif ( ! empty( $order ) && 'paypalstandard' === $order->gateway ) {
+		if ( ! empty( $_POST['txn_type'] ) && 'subscr_failed' === $_POST['txn_type'] ) {
+			// Payment failed, so we're past due. No extension.
+			$pmpro_next_payment_timestamp = false;
+		} else {
+			// Use the built in PMPro function to guess next payment date.
+			$pmpro_next_payment_timestamp = pmpro_next_payment( $user_id );
+		}
 	} elseif ( ! empty( $order ) && 'paypalexpress' === $order->gateway ) {
 		// Check the transaction type.
 		if ( ! empty( $_POST['txn_type'] ) && in_array( $_POST['txn_type'], [
 				'recurring_payment_failed',
 				'recurring_payment_skipped',
+				'recurring_payment_suspended',
 				'recurring_payment_suspended_due_to_max_failed_payment'
 			] ) ) {
 			// Payment failed, so we're past due. No extension.
